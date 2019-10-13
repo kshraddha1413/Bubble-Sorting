c24ce85d630634b7b46884f036bba6d6304c4662
Travis Lewis <travis.of.lewis@gmail.com>
2019-10-12 22:23:21 -0400

    first commit

---
+++ src/sort.java
@@ -0,0 +1,22 @@
+
+public class sort {
+
+	public static void main(String[] args) {
+		int[] arr= {23,49,3,5,6,1};
+		int temp = 0;
+		for (int i = 0; i < arr.length-1; i++) {
+			for (int j = 0; j < arr.length-i-1; j++) {
+				if(arr[j]>arr[j+1]) {
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;}
+
+			}
+		}
+		for(int i=0;i<arr.length;i++) {
+			System.out.println("sorted:"+arr[i]);
+		}
+
+	}
+
+}
